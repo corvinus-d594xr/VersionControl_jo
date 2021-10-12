ad908e95e07296111b5183f3c00286f570a7f175
corvinus-d594xr <annamaria.madly@stud.uni-corvinus.hu>
2021-10-12 14:11:21 +0200

    Add Excel namespace

--- ExcelExport/ExcelExport/Form1.cs
+++ ExcelExport/ExcelExport/Form1.cs
@@ -12,9 +12,17 @@ namespace ExcelExport
 {
     public partial class Form1 : Form
     {
+        RealEstateEntities context = new RealEstateEntities(); //A Form1 osztály szintjén példányosítsd az ORM objektumot! (Ha nem írtad át, akkor RealEstateEntities néven kellett létrejöjjön.)
+        List<Flat> lakasok; //A Form1 osztály szintjén hozz létre egy Flat típusú elemekből álló listára mutató referenciát. (Nem kell inicializálni new-val.)
         public Form1()
         {
             InitializeComponent();
+            LoadData();
+            dataGridView1.DataSource = lakasok;
+        }
+        public void LoadData() //Hozz létre egy void visszatérésű értékű, paraméter nélküli függvényt a Form1 osztályon belül LoadData néven, és hívd meg a Form konstruktorából.
+        {
+            lakasok = context.Flats.ToList();
         }
     }
 }

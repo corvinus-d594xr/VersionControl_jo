412e6ebc247de7ec0181dc9297d08cf2101979b4
corvinus-d594xr <annamaria.madly@stud.uni-corvinus.hu>
2021-11-12 23:17:32 +0100

    Car.cs, image

---
+++ 7gyak_D594XR/7gyak_D594XR/Entities/Car.cs
@@ -0,0 +1,19 @@
+﻿using _7gyak_D594XR.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7gyak_D594XR.Entities
+{
+    public class Car : Toy
+    {
+        protected override void DrawImage(Graphics g)
+        {
+            Image imageFile = Image.FromFile("Images/car.png");
+            g.DrawImage(imageFile, new Rectangle(0, 0, Width, Height));
+        }
+    }
+}

75b57c47cafb90ad9317f8b632fc90131040a541
corvinus-d594xr <annamaria.madly@stud.uni-corvinus.hu>
2021-11-26 20:31:36 +0100

    tasks/week08 lenne de az előző házinál sikeresen szintén ezt a nevet adtam, mert nem tudok számolni

---
+++ MNBSup_D594XR/MNBSup_D594XR/Form2.cs
@@ -0,0 +1,25 @@
+﻿using MNBSup_D594XR.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MNBSup_D594XR
+{
+    public partial class Form2 : Form
+    {
+        BindingList<RateData> Rates = new BindingList<RateData>();
+        public Form2()
+        {
+            InitializeComponent();
+            var er1 = from x in Rates
+                      select x;
+            dataGridView1.DataSource = er1.ToList();
+        }
+    }
+}

26bc1c628ea2702374e903a4a1e1c4f785a60245
corvinus-d594xr <annamaria.madly@stud.uni-corvinus.hu>
2021-12-02 15:25:26 +0100

    New .cs files

---
+++ Mikro_D594XR/Mikro_D594XR/Entities/BirthProbability.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mikro_D594XR.Entities
+{
+    public class BirthProbability
+    {
+        public Gender Gender { get; set; }
+        public int Age { get; set; }
+        public bool P { get; set; }
+    }
+}

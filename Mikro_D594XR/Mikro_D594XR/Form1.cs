e59469f83ece54684b9fd06d046df4ed4a423d41
corvinus-d594xr <annamaria.madly@stud.uni-corvinus.hu>
2021-12-03 20:26:33 +0100

    rng

--- Mikro_D594XR/Mikro_D594XR/Form1.cs
+++ Mikro_D594XR/Mikro_D594XR/Form1.cs
@@ -14,6 +14,7 @@ namespace Mikro_D594XR
 {
     public partial class Form1 : Form
     {
+        Random rng = new Random();
         List<Person> Population = null;
         List<BirthProbability> BirthProbabilities = null;
         List<DeathProbability> DeathProbabilities = null;
@@ -23,6 +24,62 @@ namespace Mikro_D594XR
             Population = GetPopulation(@"C:\Temp\nép.teszt.csv");
             BirthProbabilities = GetBirthProbabilities(@"C:\Temp\születés.csv");
             DeathProbabilities = GetDeathProbabilities(@"C:\Temp\halál.csv");
+
+            // Végigmegyünk a vizsgált éveken
+            for (int year = 2005; year <= 2024; year++)
+            {
+                // Végigmegyünk az összes személyen
+                for (int i = 0; i < Population.Count; i++)
+                {
+                    // Ide jön a szimulációs lépés
+                    SimStep(year,Population[i]);
+                }
+
+                int nbrOfMales = (from x in Population
+                                  where x.Gender == Gender.Male && x.IsAlive
+                                  select x).Count();
+                int nbrOfFemales = (from x in Population
+                                    where x.Gender == Gender.Female && x.IsAlive
+                                    select x).Count();
+                Console.WriteLine(
+                    string.Format("Év:{0} Fiúk:{1} Lányok:{2}", year, nbrOfMales, nbrOfFemales));
+            }
+        }
+
+        private void SimStep(int year, Person person)
+        {
+            //Ha halott akkor kihagyjuk, ugrunk a ciklus következő lépésére
+            if (!person.IsAlive) return;
+
+            // Letároljuk az életkort, hogy ne kelljen mindenhol újraszámolni
+            byte age = (byte)(year - person.BirthYear);
+
+            // Halál kezelése
+            // Halálozási valószínűség kikeresése
+            double pDeath = (from x in DeathProbabilities
+                             where x.Gender == person.Gender && x.Age == age
+                             select x.P).FirstOrDefault();
+            // Meghal a személy?
+            if (rng.NextDouble() <= pDeath)
+                person.IsAlive = false;
+
+            //Születés kezelése - csak az élő nők szülnek
+            if (person.IsAlive && person.Gender == Gender.Female)
+            {
+                //Szülési valószínűség kikeresése
+                double pBirth = (from x in BirthProbabilities
+                                 where x.Age == age
+                                 select x.P).FirstOrDefault();
+                //Születik gyermek?
+                if (rng.NextDouble() <= pBirth)
+                {
+                    Person újszülött = new Person();
+                    újszülött.BirthYear = year;
+                    újszülött.NbrOfChildren = 0;
+                    újszülött.Gender = (Gender)(rng.Next(1, 3));
+                    Population.Add(újszülött);
+                }
+            }
         }
 
         public List<Person> GetPopulation(string csvpath)
@@ -48,14 +105,14 @@ namespace Mikro_D594XR
 
         public List<BirthProbability> GetBirthProbabilities(string csvpath)
         {
-            List<BirthProbability> birthProbability = new List<BirthProbability>();
+            List<BirthProbability> birthProbabilities = new List<BirthProbability>();
 
             using (StreamReader sr = new StreamReader(csvpath, Encoding.Default))
             {
                 while (!sr.EndOfStream)
                 {
                     var line = sr.ReadLine().Split(';');
-                    birthProbability.Add(new BirthProbability()
+                    birthProbabilities.Add(new BirthProbability()
                     {
                         Age = int.Parse(line[0]),
                         NbrOfChildren = int.Parse(line[1]),
@@ -64,28 +121,28 @@ namespace Mikro_D594XR
                 }
             }
 
-            return birthProbability;
+            return birthProbabilities;
         }
 
         public List<DeathProbability> GetDeathProbabilities(string csvpath)
         {
-            List<DeathProbability> population = new List<DeathProbability>();
+            List<DeathProbability> deathProbabilities = new List<DeathProbability>();
 
             using (StreamReader sr = new StreamReader(csvpath, Encoding.Default))
             {
                 while (!sr.EndOfStream)
                 {
                     var line = sr.ReadLine().Split(';');
-                    population.Add(new DeathProbability()
+                    deathProbabilities.Add(new DeathProbability()
                     {
                         Gender = (Gender)Enum.Parse(typeof(Gender), line[0]),
                         Age = int.Parse(line[1]),
-                        P = double.Parse(line[2])
+                        P = double.Parse(line[2].Replace(",","."))
                     });
                 }
             }
 
-            return population;
+            return deathProbabilities;
         }
 
     }
